3b44a515747ab06088506358f0ede1ccd2d3f6ce
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2024-01-09 18:36:54 -0300

    feat: adicionando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    CommonModule,
+    MenuComponent,
+    FooterComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
 })
 export class AppComponent {
   title = 'AppAngular';
---
+++ src/app/navegacao/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './menu.component.html',
+})
+export class MenuComponent {
+  home: any;
+}
